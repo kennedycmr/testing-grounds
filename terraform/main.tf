3bb3c1b97325b089091b22fc9e15b2178cb1cc04
Cameron Kennedy <kennedycmr@gmail.com>
2024-03-12 20:33:47 -0400

    adding in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,9 @@
 data "google_project" "gcp_project" {
   project_id = var.GCP_PROJECT_ID
 }
+
+# Create service account
+resource "google_service_account" "service_account" {
+  account_id   = "test-service-account"
+  display_name = "Test Service Account"
+}
